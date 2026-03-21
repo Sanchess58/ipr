25ed0016c6f05d8472aab4b0d12a18280c2aa0a5
Александр Сотников <aleksandr.sotnikov@osinit.com>
2026-03-21 13:45:11 +0300

    python collections add

---
+++ python/collections/tuple_collection.py
@@ -0,0 +1,4 @@
+tuple_ = tuple()
+
+tuple_.count() # Возвращает количество вхождений переданного объекта. time complexity O(N)
+tuple_.index() # Возвращает первый индекс на котором находится переданное число. time complexity O(N)
